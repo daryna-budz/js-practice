8ca0ab660e934bf0b7d674ca841cedd772f8c9cc
daryna-budz <darynabudz@gmail.com>
2024-10-20 20:33:06 -0400

    variables and data types task

---
+++ task-1/script.js
@@ -0,0 +1,9 @@
+const userName = prompt("What is your name?");
+console.log(userName);
+
+const userAge = prompt("What is your age?").toString();
+console.log(typeof userAge);
+
+let num = 10;
+num += "20";
+console.log(num, typeof num)
